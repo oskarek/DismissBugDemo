1db2c17021f79dd06b14be666a3a630f76d8e7e6
Oskar Ek <oskar.ek@me.com>
2024-04-12 13:06:56 +0200

    Initial Commit

---
+++ DismissBugDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DismissBugDemo
+//
+//  Created by Oskar Ek on 2024-04-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DismissBugDemo/DismissBugDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DismissBugDemoApp.swift
+//  DismissBugDemo
+//
+//  Created by Oskar Ek on 2024-04-12.
+//
+
+import SwiftUI
+
+@main
+struct DismissBugDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
